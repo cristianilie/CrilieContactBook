10855e45841e600087c20b36ab3afa222f0622f4
Ilie Cristian Marius <Ilie_Cristian_Marius@hotmail.com>
2018-11-24 20:11:41 +0200

    Converted the enum bindings.

--- CrilieContactBook/ViewModels/Enums/EventListFilter.cs
+++ CrilieContactBook/ViewModels/Enums/EventListFilter.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CrilieContactBook.ViewModels
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum EventListFilter
     {
+        [Description("Today")]
         Today,
+        [Description("Next 3 days")]
         On_3_Days,
+        [Description("Next 7 days")]
         On_7_Days,
+        [Description("Next 14 days")]
         On_14_Days,
+        [Description("Next 30 days")]
         On_30_Days,
+        [Description("All Active")]
         All_Active,
+        [Description("Inactive")]
         InactiveEvents
     }
 }
--- CrilieContactBook/ViewModels/Enums/TaskListFilter.cs
+++ CrilieContactBook/ViewModels/Enums/TaskListFilter.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CrilieContactBook.ViewModels
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum TaskListFilter
     {
+        [Description("All Tasks")]
         All,
+        [Description("Active Tasks")]
         Active,
+        [Description("Completed Tasks")]
         Completed,
+        [Description("Failed Tasks")]
         Failed
     }
 }
